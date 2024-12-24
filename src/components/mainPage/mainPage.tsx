f3cce0398f5ebda47f0b97b7470c425081f25cd8
M4rrt <guilhermemartinszot@gmail.com>
2024-12-24 11:48:43 -0300

    CharacterSheet

--- src/components/mainPage/mainPage.tsx
+++ src/components/mainPage/mainPage.tsx
@@ -18,18 +18,30 @@ export const MainPage: FC<IMainPage> = ({ charactersList }) => {
     const lastChar = charactersList?.find((c) => c.id === +lastViewed)
 
     return <Row style={{ justifyContent: "space-evenly", alignItems: "center" }}>
-        <Col span={4}  onClick={()=>navigate('/Biblioteca')}style={{ border: '1px solid black', justifyContent: "center", placeContent: "center", textAlign: 'center', height: '55vh', backgroundColor: "#E3E1E6", borderRadius: 10 }}>
+        <Col span={4} onClick={() => navigate('/Biblioteca')} style={{ border: '1px solid black', justifyContent: "center", placeContent: "center", textAlign: 'center', height: '55vh', backgroundColor: "#E3E1E6", borderRadius: 10 }}>
             <Col style={{ height: "75%", backgroundImage: `url(${randomPatron()}`, backgroundPosition: "center center", backgroundSize: "cover", borderTopLeftRadius: 10, borderTopRightRadius: 10 }}>
                 <Col style={{ height: "100%", alignContent: "center", width: '100%', backgroundColor: 'rgb(238, 238, 238, 0.5)', borderTopLeftRadius: 10, borderTopRightRadius: 10 }}>
                     {iconLibrary()}
                 </Col>
             </Col>
-            <Col  style={{ height: "25%", fontSize: 20, alignContent: "center" }}>
+            <Col style={{ height: "25%", fontSize: 20, alignContent: "center" }}>
                 Biblioteca
             </Col>
         </Col>
-        <Col span={6} style={{ border: '1px solid black', justifyContent: "center", placeContent: "center", textAlign: 'center', height: '70vh', backgroundColor: "#E3E1E6", borderRadius: 15 }}>
-            <Col style={{ height: '80%', backgroundImage: lastChar ? `url(${lastChar.image})` : `url(${randomPatron()}`, backgroundSize: "cover", backgroundPosition : "center", borderTopLeftRadius: 10, borderTopRightRadius: 10 }}></Col>
+        <Col span={6}
+            onClick={()=> {lastChar && navigate(`/Ficha/${lastChar.id}`)}}
+            style={
+                {
+                    border: '1px solid black',
+                    justifyContent: "center",
+                    placeContent: "center",
+                    textAlign: 'center',
+                    height: '70vh',
+                    backgroundColor: "#E3E1E6",
+                    borderRadius: 15
+                }}
+        >
+            <Col style={{ height: '80%', backgroundImage: lastChar ? `url(${lastChar.image})` : `url(${randomPatron()}`, backgroundSize: "cover", backgroundPosition: "center", borderTopLeftRadius: 10, borderTopRightRadius: 10 }}></Col>
             <Col style={{ fontSize: 20, height: '20%', alignContent: 'center' }}>
                 {lastChar ? lastChar.name : "Sem Personagem Selecionado"}
                 <Col>
@@ -51,6 +63,6 @@ export const MainPage: FC<IMainPage> = ({ charactersList }) => {
                     <span style={{ fontSize: 13 }}>Personagem</span>
                 </Col>
             </Col>
-        </Col>  
+        </Col>
     </Row>
 }
--- src/components/sheet/sheet.tsx
+++ src/components/sheet/sheet.tsx
@@ -1,33 +1,72 @@
-import { Button, Col, Image, Row, Tabs, TabsProps } from "antd"
-import { useEffect, useState } from "react"
+import { Button, Col, Image, Input, InputNumber, Row, Tabs, TabsProps, Tooltip } from "antd"
+import { useEffect, useMemo, useState } from "react"
 import { useNavigate, useParams } from "react-router"
 import { db } from "../db/db"
 import { useLiveQuery } from "dexie-react-hooks"
-import { CharacterType } from "../../types/CharacterType"
+import { AttributeData, CharacterType, Pericia } from "../../types/CharacterType"
 import { StatusBar } from "./StatusBar/statusBar"
-import { MOCK_ATTRIBUTES, rankToTrainingModifier } from "../../util/util"
-import { SkillsRender } from "./skillsRender/skillsRender"
+import { attributeModifier, MOCK_ATTRIBUTES, rankToTrainingModifier } from "../../util/util"
+import { SkillsTable } from "./skillsRender/skillsTable"
+import { AttributesTable } from "./attributesTable/attributesTable"
+import logo from '../../images/logo.png';
 
 
 export const Sheet = () => {
     const navigate = useNavigate()
     const { id } = useParams()
     const character_id = id || 0
-
+    const [editSheet, setEditSheet] = useState(false)
+    const [mainColor, setMainColor] = useState("#076b07")
+    const [attributes, setAttributes] = useState<AttributeData[]>(MOCK_ATTRIBUTES.map((attribute) => {
+        const updatedPericias = attribute.pericias.map((pericia) => {
+            return {
+                ...pericia,
+                attMod: attributeModifier(attribute.Attribute),
+                treined: false,
+                bonus: 0
+            } as Pericia
+        })
+        return { ...attribute, pericias: updatedPericias }
+    }))
+    
     const charactersList = useLiveQuery(
         async () => {
             const characters = await db.characters
+                // eslint-disable-next-line no-useless-computed-key
                 .where({ ["id"]: +character_id }).first()
             return characters
         }
     )
+
     const [character, setCharacter] = useState<CharacterType>(() => {
         if (charactersList !== undefined) {
             return charactersList
         }
         return { attributes: MOCK_ATTRIBUTES } as CharacterType
     })
+    
 
+    
+    useEffect(() => {
+        if (charactersList !== undefined) {
+            setCharacter(charactersList)
+        }
+    }, [id, charactersList])
+    
+    const actualAttributes = useMemo(() => {
+        const actual = attributes.map((attribute) => {
+            const updatedPericias = attribute.pericias.map((pericia) => {
+                return {
+                    ...pericia,
+                    attMod: attributeModifier(attribute.Attribute),
+                } as Pericia
+            })
+            return { ...attribute, pericias: updatedPericias } as AttributeData
+        })
+        return actual
+    }, [attributes])
+    
+    
     const updateCharacter = (update: Partial<CharacterType>) => {
         setCharacter(
             (prev) => {
@@ -39,37 +78,166 @@ export const Sheet = () => {
         )
     }
 
-    useEffect(() => {
-        if (charactersList !== undefined) {
-            setCharacter(charactersList)
-        }
-    }, [id, charactersList])
+    const updateSkills = (_: Pericia) => {
+        setAttributes((prev) => {
+            const newAttributes = prev.map((attribute) => {
+                const pericias = attribute.pericias.map((pericia) =>
+                    pericia.name === _.name ? _ : pericia
+                )
+                return { ...attribute, pericias } as AttributeData
+            })
+            return newAttributes as AttributeData[]
+        })
+    }
 
-    console.log(character)
+    const updateAttributes = (_: AttributeData[]) => {
+        setAttributes(_)
+    }
 
-    const Items: TabsProps['items'] = [
-        {
-            key: 'atributos',
-            label: 'Atributos',
-            children: <>
-                <SkillsRender attributes={character.attributes} updatePericia={()=> {}} trainingMod={rankToTrainingModifier(character.rank)} />
+    const updateMainColor = (_: string) => {
+        setMainColor(_)
+    }
 
-            </>
-        },
-        {
-            key: 'runas',
-            label: 'Runas',
-            children: <>
-                Runas
-            </>
-        },
+    return (
+        <Col span={24}
+            style={{
+                height: "100%",
+                backgroundColor: "rgb(227, 225, 230, 0.95)",
+                padding: "40px 50px 50px 75px"
+            }}
+        >
+            <Row gutter={50}>
+                <Col span={6}>
+                    <Image style={{ height: '65vh', borderRadius: 10, objectFit: 'cover' }} preview={false} src={character.image} alt="" />
+                    {character && <StatusBar character={character} characterUpdate={updateCharacter} />}
+                </Col>
+                <Col span={18}>
+                    <Row>
+                        <Col span={20} style={{ border: "1px solid black", borderRadius: 8 }}>
+                            {editSheet && (
+                                    <Row>
+                                    <Col span={6} style={{ paddingTop: 22, paddingLeft: 11 }}>
+                                        <Row>
+                                            <Col span={24}>
+                                                <Row style={{ fontSize: 10, alignItems: 'center', placeContent: 'space-between' }}>
+                                                    <Col span={10}>
+                                                        Jogador:
+                                                    </Col>
+                                                    <Col span={14}>
+                                                        <Input onChange={(e)=> updateCharacter({ playerName : e.target.value})} className={'purple-shadow'} size="small" style={{ backgroundColor: "transparent", border: '1px solid black', fontSize: 10 }} placeholder="Nome do Jogador"></Input>
+                                                    </Col>
+                                                </Row>
+                                            </Col>
+                                            <Col span={24} style={{ paddingTop: 21 }}>
+                                                <Row style={{ fontSize: 10, alignItems: 'center', placeContent: 'space-between', }}>
+                                                    <Col span={10}>
+                                                        Dinheiro:
+                                                    </Col>
+                                                    <Col span={14}>
+                                                        <InputNumber min={0} onChange={(e)=> e!==null ? updateCharacter({ money : e}) : null} className={'purple-shadow'} size="small" style={{ backgroundColor: "transparent", border: '1px solid black', fontSize: 10 }} placeholder="Dinheiro na Conta"></InputNumber>
+                                                    </Col>
+                                                </Row>
+            
+                                            </Col>
+                                        </Row>
+                                    </Col>
+                                    <Col offset={1} span={6} style={{ paddingTop: 22 }}>
+                                        <Row>
+                                            <Col span={24}>
+                                                <Row style={{ fontSize: 10, alignItems: 'center', placeContent: 'space-between' }}>
+                                                    <Col span={10}>
+                                                        Nome:
+                                                    </Col>
+                                                    <Col span={14}>
+                                                        <Input onChange={(e)=> updateCharacter({ name: e.target.value})} className={'purple-shadow'} size="small" style={{ backgroundColor: "transparent", border: '1px solid black', fontSize: 10 }} placeholder="Nome do Personagem"></Input>
+                                                    </Col>
+                                                </Row>
+                                            </Col>
+                                            <Col span={24} style={{ paddingTop: 21 }}>
+                                                <Row style={{ fontSize: 10, alignItems: 'center', placeContent: 'space-between', }}>
+                                                    <Col span={10}>
+                                                        Peso:
+                                                    </Col>
+                                                    <Col span={14}>
+                                                        <Input onChange={(e)=> updateCharacter({ weight: e.target.value})} className={'purple-shadow'} size="small" style={{ backgroundColor: "transparent", border: '1px solid black', fontSize: 10 }} placeholder="Peso"></Input>
+                                                    </Col>
+                                                </Row>
+            
+                                            </Col>
+                                        </Row>
+                                    </Col>
+                                    <Col offset={1} span={6} style={{ paddingTop: 22 }}>
+                                        <Row>
+                                            <Col span={24}>
+                                                <Row style={{ fontSize: 10, alignItems: 'center', placeContent: 'space-between' }}>
+                                                    <Col span={10}>
+                                                        Idade:
+                                                    </Col>
+                                                    <Col span={14}>
+                                                        <InputNumber  min={0} onChange={(e)=> e !== null ? updateCharacter({ age: e }) : null} className={'purple-shadow'} size="small" style={{ backgroundColor: "transparent", border: '1px solid black', fontSize: 10 }} placeholder="Idade"></InputNumber>
+                                                    </Col>
+                                                </Row>
+                                            </Col>
+                                            <Col span={24} style={{ paddingTop: 21 }}>
+                                                <Row style={{ fontSize: 10, alignItems: 'center', placeContent: 'space-between', }}>
+                                                    <Col span={10}>
+                                                        Altura:
+                                                    </Col>
+                                                    <Col span={14}>
+                                                        <Input onChange={(e)=> updateCharacter({ height: e.target.value})}  className={'purple-shadow'} size="small" style={{ backgroundColor: "transparent", border: '1px solid black', fontSize: 10 }} placeholder="Altura"></Input>
+                                                    </Col>
+                                                </Row>
+            
+                                            </Col>
+                                        </Row>
+                                    </Col>
+                                </Row>
+                
+                            )
+                            }
+                            {!editSheet && (
+                                <Row gutter={20} style={{ padding: "20px 28px 20px 20px" }}>
+                                    <Col span={8}>
+                                        <Button className="purple-shadow black-hover btn" style={{ width: "100%", fontSize: 20, borderRadius: 8, backgroundColor: "transparent" }} size="large">Runas</Button>
+                                    </Col>
+                                    <Col span={8}>
+                                        <Button className="purple-shadow black-hover btn" style={{ width: "100%", fontSize: 20, borderRadius: 8, backgroundColor: "transparent" }} size="large">Anotações</Button>
+                                    </Col>
+                                    <Col span={8}>
+                                        <Button className="purple-shadow black-hover btn" style={{ width: "100%", fontSize: 20, borderRadius: 8, backgroundColor: "transparent" }} size="large">Progressão</Button>
+                                    </Col>
+                                </Row>
+                            )}
+                        </Col>
+                        <Col span={44} style={{ justifyItems: "self-end", textAlign: "center" }}>
+                                        <div style={{ marginRight: 22 }}>
+                                            <Tooltip title={<>Click Aqui Para Voltar</>} >
+                                            <Image onClick={()=>window.history.back()}src={logo} height={53} preview={false}/>
+                                            </Tooltip>
+                                            <br />
+                                            <Button
+                                            //  disabled={!canSave}
+                                              size="small"
+                                               style={{ fontSize: 10, border: "1px solid black", background: 'transparent', borderRadius: 20, width: 72, marginTop: 7 }} 
+                                            //    onClick={() => SaveCharacterSheet()}
+                                               >
+                                                <span style={{ borderBottom: -2, }}>
+                                                    Salvar
+                                                </span>
+                                            </Button>
+            
+                                        </div>
+                                    </Col>
+                        <Col span={24} style={{ border: "1px solid black", borderRadius: 8, marginTop: 20 }}>
+                            {editSheet && <AttributesTable attributes={{ value: actualAttributes, updateAttributes: updateAttributes }} color={{ value: mainColor, setColor: updateMainColor }} />}
+                        </Col>
+                        <Col span={24}>
+                            <SkillsTable skills={actualAttributes} updatePericia={updateSkills} trainingMod={rankToTrainingModifier(character.rank)} />
+                        </Col>
+                    </Row>
+                </Col>
 
-    ]
-
-    return <Row>
-        {character && <StatusBar character={character} characterUpdate={updateCharacter} />}
-        <Tabs style={{width : '100%'}} defaultActiveKey="atributos" items={Items}></Tabs>
-
-        {character?.age === undefined && <Button onClick={() => navigate('/')}>Voltar</Button>}
-    </Row>
+            </Row >
+        </Col >
+    )
 }
